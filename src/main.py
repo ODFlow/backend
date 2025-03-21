e84350d1532aa80e8845843f7a034dfb91e1b061
dstring <dstring2@proton.me>
2025-03-21 16:00:15 +0200

    feat: ip rate limiting, data refresh Refs #5, #19

--- src/main.py
+++ src/main.py
@@ -33,7 +33,50 @@ scheduler.add_job(cron_job,
                   name="Update all data")
 
 schema = strawberry.federation.Schema(query=Query)
-graphql_app = GraphQLRouter(schema=schema)
+redis_client = redis.StrictRedis(host='localhost', port=6379, db=0)
+
+limiter = Limiter(key_func=get_remote_address,
+                  storage_uri="redis://localhost:6379/0")
+app = FastAPI()
+app.state.limiter = limiter
+app.add_exception_handler(RateLimitExceeded, _rate_limit_exceeded_handler)
+
+
+update_key = 'update_key' # should be changed, will be moved to .env
+update_url = 'update' # should be changed, will be moved to .env
+
+
+class TriggerUpdate(BaseModel):
+    key: str
+
+
+@app.post(f"/trigger-update/{update_url}")
+async def trigger_fetch(t: TriggerUpdate, background_tasks: BackgroundTasks):
+    if t.key != update_key:
+        return JSONResponse(status_code=400, content={"message": "Invalid Key"})
+
+    background_tasks.add_task(cron_job)
+    return {"message": "Success"}
+
+
+@limiter.limit("60/minute")
+async def check_rate_limit(request: Request):
+    return True
+
+
+@app.exception_handler(RateLimitExceeded)
+async def rate_limit_exception(request: Request, exc: RateLimitExceeded):
+    return JSONResponse(
+        status_code=429,
+        content={
+            "message":
+                "Rate limit exceeded. "
+                f"Please try again in {int(exc.limit.limit.get_expiry() / 60)} mins",
+        })
+
+
+graphql_app = GraphQLRouter(schema=schema,
+                            dependencies=[Depends(check_rate_limit)])
 
 app.include_router(router=graphql_app, prefix='/graphql')
 
